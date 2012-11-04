527d9b1c7eefe78575d129eb11e4f4dabea595c0
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2012-11-04 22:31:39 +0000

    Former-commit-id: ed48e43bee0b7623745e4911b4ddcc3e4fc9a06f

--- Kinslayer Web/src/org/kinslayermud/web/actions/StoreAction.java
+++ Kinslayer Web/src/org/kinslayermud/web/actions/StoreAction.java
@@ -1,18 +1,14 @@
 package org.kinslayermud.web.actions;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
-
-import org.kinslayermud.cart.Cart;
-import org.kinslayermud.util.MiscUtil;
 import org.kinslayermud.util.WebSupport;
 import org.kinslayermud.util.WebSupportImp;
+
 import com.opensymphony.xwork2.ActionSupport;
-import org.kinslayermud.web.util.HttpUtil;
 
 public abstract class StoreAction extends ActionSupport implements ServletRequestAware, ServletResponseAware {
 
@@ -28,48 +24,11 @@ public abstract class StoreAction extends ActionSupport implements ServletReques
     webSupport = new WebSupportImp();
   }
   
-  public abstract String execute(Cart cart) throws Exception;
+  public abstract String execute(WebSupport webSupport) throws Exception;
   
   public String execute() throws Exception {
-    
-    Cart cart = null;
-    
-    Cookie cartIdCookie = HttpUtil.getCookieByName(request, CART_ID_COOKIE_NAME);
-    Cookie cartRandomIdCookie = HttpUtil.getCookieByName(request, CART_RANDOM_ID_COOKIE_NAME);
-    
-    if(cartIdCookie != null && cartRandomIdCookie != null) {
-      
-      int cartId;
-      String cartRandomId;
-      
-      if(MiscUtil.isValidIntString(cartIdCookie.getValue())) {
-        
-        cartId = Integer.valueOf(cartIdCookie.getValue());
-        cartRandomId = cartRandomIdCookie.getValue();
-        
-        cart = webSupport.getCart(cartId);
-        
-        if(cart != null && !cart.getRandomId().equals(cartRandomId)) {
-          
-          cart = null;//Mismatch. Build a new cart.
-        }
-      }
-    }
-    
-    if(cart == null) {
-      
-      cart = webSupport.createNewCart(request.getRemoteAddr());
-      
-      cartIdCookie = new Cookie(CART_ID_COOKIE_NAME, String.valueOf(cart.getId()));
-      cartRandomIdCookie = new Cookie(CART_RANDOM_ID_COOKIE_NAME, cart.getRandomId());
-      
-      response.addCookie(cartIdCookie);
-      response.addCookie(cartRandomIdCookie);
-    }
-    
-    System.out.println("Store Action. Cart Found: " + cart.getId());
-    
-    return execute(cart);
+  
+    return execute(webSupport);
   }
   
   public void setServletRequest(HttpServletRequest request) {
