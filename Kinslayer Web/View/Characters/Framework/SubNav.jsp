a76ca1cb1eea66e7f4a975c5c071af2ed906787a
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2012-11-05 02:38:01 +0000

    Former-commit-id: 0f203092829809aad6c1198b4dce896853febe25

---
+++ Kinslayer Web/View/Characters/Framework/SubNav.jsp
@@ -0,0 +1,11 @@
+
+<div class="subnavContainer">
+	<ul class="subnav">
+		<li class="subnav">
+			<a href="./character-classes">Character Classes</a>
+		</li>
+		<li class="subnav">
+			<a href="./character-skills">Character Skills</a>
+		</li>
+	</ul>
+</div>
---
+++ Kinslayer Web/src/org/kinslayermud/web/characters/CharacterClassesAction.java
@@ -0,0 +1,13 @@
+package org.kinslayermud.web.characters;
+
+import org.kinslayermud.util.WebSupport;
+import org.kinslayermud.web.actions.StandardAction;
+
+public class CharacterClassesAction extends StandardAction {
+
+  protected String SUCCESS_FORWARD = "Success";
+  public String execute(WebSupport webSupport) throws Exception {
+
+    return SUCCESS_FORWARD;
+  }
+}
