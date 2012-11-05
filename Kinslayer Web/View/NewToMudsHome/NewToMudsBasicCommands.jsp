ec7f4a8bf5f1ac5e2f2018a3be57996604afc56d
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2012-11-05 02:05:40 +0000

    Former-commit-id: e99f0eb646f64f521b8ba3b5fa1e87010bd0ef7b

---
+++ Kinslayer Web/View/NewToMudsHome/NewToMudsBasicCommands.jsp
@@ -0,0 +1,18 @@
+
+	<div class="container_box">
+	
+		<div style="margin: 20px; float:left;">
+		
+			<div>
+			
+			<h1>
+				Basic Commands
+			</h1>
+			
+			<p>
+				Content
+			</p>
+			</div>
+		</div>
+		<div style="clear: both;"></div>
+	</div> <!-- End of Container Box -->
---
+++ Kinslayer Web/src/org/kinslayermud/web/newtomuds/NewToMudsCharacterCreationAction.java
@@ -0,0 +1,13 @@
+package org.kinslayermud.web.newtomuds;
+
+import org.kinslayermud.util.WebSupport;
+import org.kinslayermud.web.actions.StoreAction;
+
+public class NewToMudsCharacterCreationAction extends StoreAction {
+
+  protected String SUCCESS_FORWARD = "Success";
+  public String execute(WebSupport webSupport) throws Exception {
+
+    return SUCCESS_FORWARD;
+  }
+}
