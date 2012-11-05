40354444d348fb81baa56422b5a3e591931873fe
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2012-11-05 08:34:59 +0000

    Former-commit-id: f349241dfb6188522bd63096b31a1eecc906cc48

--- Kinslayer Web/src/org/kinslayermud/web/playerportal/signedin/ViewUserLogAction.java
+++ Kinslayer Web/src/org/kinslayermud/web/playerportal/signedin/ViewUserLogAction.java
@@ -1,11 +1,9 @@
 package org.kinslayermud.web.playerportal.signedin;
 
-import java.util.List;
-
 import org.kinslayermud.character.User;
 import org.kinslayermud.userlog.UserLog;
-import org.kinslayermud.userlog.UserLogRecord;
 import org.kinslayermud.util.MiscUtil;
+import org.kinslayermud.util.StringUtil;
 import org.kinslayermud.util.WebSupport;
 
 public class ViewUserLogAction extends ValidateSignInAction {
@@ -25,6 +23,7 @@ public class ViewUserLogAction extends ValidateSignInAction {
       
       if(userLog.getUserId() == user.getUserId()) {
         
+        userLog.setConsoleOutput(formatUserLog(userLog.getConsoleOutput()));
         request.setAttribute("UserLog", userLog);
         return SUCCESS_FORWARD;
       }
@@ -32,4 +31,44 @@ public class ViewUserLogAction extends ValidateSignInAction {
     
     return FAILURE_FORWARD;
   }
+  
+  public String formatUserLog(String consoleBuffer) {
+    
+    StringBuffer formattedLog = new StringBuffer();
+    StringBuffer numberTextStringBuffer = new StringBuffer();
+    int tempIndex;
+    
+    for(int index = 0;index < consoleBuffer.length();++index) {
+      
+      if(consoleBuffer.charAt(index) == 0x1B) {
+        
+        tempIndex = index + 1;
+        if(index < consoleBuffer.length() && consoleBuffer.charAt(index) == '[') {
+          
+          numberTextStringBuffer = new StringBuffer();
+          ++tempIndex;
+          
+          while(tempIndex < consoleBuffer.length() && Character.isDigit(consoleBuffer.charAt(tempIndex))) {
+            
+            numberTextStringBuffer.append(consoleBuffer.charAt(tempIndex));
+            
+            ++tempIndex;
+          }
+          
+          if(numberTextStringBuffer.length() > 0 && tempIndex < consoleBuffer.length() && consoleBuffer.charAt(tempIndex) == 'm') {
+            
+            int colorCode = Integer.valueOf(numberTextStringBuffer.toString());
+            index = tempIndex;
+          }
+        }
+      }
+      else {
+        
+        formattedLog.append(consoleBuffer.charAt(index));
+      }
+    }
+
+    return StringUtil.ConvertToHTML(formattedLog.toString());
+  }
+  
 }
