11a2c539b35cd6b812aa5df47af67f682835df19
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-01-24 01:48:06 +0000

    * Delete old projects
    Former-commit-id: d95055ba62b637a01d7e42ba30d5056dfddc84cd

--- Kinslayer Shared/src/org/kinslayermud/exception/NonExistentObjectException.java
+++
@@ -1,9 +0,0 @@
-package org.kinslayermud.exception;
-
-public class NonExistentObjectException extends Exception {
-
-  public NonExistentObjectException(String reason) {
-    super(reason);
-  }
-}
-
--- Kinslayer Web/src-core/org/kinslayermud/scripts/KinslayerProcess.java
+++
@@ -1,8 +0,0 @@
-package org.kinslayermud.scripts;
-
-import org.kinslayermud.misc.Provider;
-
-public interface KinslayerProcess {
-  
-  public void process(Provider provider);
-}
