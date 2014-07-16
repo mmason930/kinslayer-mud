543c42ba0492de4f335d42d673e65311078533cd
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-16 01:49:57 +0000

    * Debug.
    Former-commit-id: 4abd36f9283b8a07b184b5db61416d63108bae7c

--- kmud-live/scripts/lib/-13277_bootOLC.js
+++ kmud-live/scripts/lib/-13277_bootOLC.js
@@ -12,16 +12,22 @@ var vOLC = [];
 function bootOLC()
 {
 	vOLC=[];
+	mudLog(constants.BRF, 100, "Booting Pedit.");
 	if( typeof bootPeditOLC != "undefined" )
 		bootPeditOLC();
+	mudLog(constants.BRF, 100, "Booting Quedit.");
 	if( typeof bootQueditOLC != "undefined" )
 		bootQueditOLC();
+	mudLog(constants.BRF, 100, "Booting Achedit.");
 	if( typeof bootAcheditOLC != "undefined" )
 		bootAcheditOLC();
+	mudLog(constants.BRF, 100, "Booting Gatedit.");
 	if( typeof bootGateditOLC != "undefined" )
 		bootGateditOLC();
+	mudLog(constants.BRF, 100, "Booting Recedit.");
 	if( typeof bootReceditOLC != "undefined" )
 		bootReceditOLC();
+	mudLog(constants.BRF, 100, "Booting Helpedit.");
 	if( typeof bootHelpeditOLC != "undefined" )
 		bootHelpeditOLC();
 }
