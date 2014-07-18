50827111b23f5c898cd2189ca1b9e7ca1778f095
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-18 21:50:54 +0000

    * New clone function.
    Former-commit-id: 872e35747eeeca9644dec3727409ae20c1c9e607

--- kmud-live/scripts/lib/-80_clone.js
+++ kmud-live/scripts/lib/-80_clone.js
@@ -1,3 +1,37 @@
+//Taken from http://stackoverflow.com/questions/728360/most-elegant-way-to-clone-a-javascript-object
+//Galnro 2014-07-18
+function clone(obj) {
+	// Handle the 3 simple types, and null or undefined
+	if (null == obj || "object" != typeof obj) return obj;
+
+	// Handle Date
+	if (obj instanceof Date) {
+		var copy = new Date();
+		copy.setTime(obj.getTime());
+		return copy;
+	}
+
+	// Handle Array
+	if (obj instanceof Array) {
+		var copy = [];
+		for (var i = 0, len = obj.length; i < len; i++) {
+			copy[i] = clone(obj[i]);
+		}
+		return copy;
+	}
+
+	// Handle Object
+	if (obj instanceof Object) {
+		var copy = {};
+		for (var attr in obj) {
+			if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+		}
+		return copy;
+	}
+
+	throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+/***
 function clone(obj){
     if(obj == null || typeof(obj) != 'object')
         return obj;
@@ -6,3 +40,4 @@ function clone(obj){
         temp[key] = clone(obj[key]);
     return temp;
 }
+***/
