33e825af8047ffe96c6a2f5df0e7d6b3ce8a48bd
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-18 04:36:38 +0000

    * Cats hunting rats.
    Former-commit-id: a78a26188b4b69a5010174aa8a8191ede9ec5280

--- kmud-live/scripts/flavor/cats/19509_CatEmotes.js
+++ kmud-live/scripts/flavor/cats/19509_CatEmotes.js
@@ -1,4 +1,27 @@
 var script19509 = function(self, actor, here, args, extra) {
+
+	//Give priority to killing rats.
+	//Galnor 2014-07-18
+
+	var rats = here.getCharacters(function(character) {
+
+		return character.vnum == 79;
+	});
+
+	if(rats.length != 0)
+	{
+		var rat = rats[0];
+
+		act("$n sniffs at something on the ground...", true, self, null, null, constants.TO_ROOM);
+
+		wait 2;
+
+		if(rat.isValid && rat.room.vnum == self.room.vnum)
+			self.hit(rat);
+
+		return;
+	}
+
 	var rand = random(1,4);
 	if( rand == 1 )
 	    here.echo(self.name + " sniffs at something on the ground...");
@@ -7,7 +30,7 @@ var script19509 = function(self, actor, here, args, extra) {
 	else if( rand == 3 )
 	{
 	    target = getRandomPersonInRoom(here);
-	    if( target != self )
+	    if( target != self && self.canSee(target) )
 	    {
 	        here.echoaround(target, self.name + " growls at " + target.name + ".");
 	        target.send(self.name + " growls at you.");
