dfdff5e440ffd8786b95b1577417a6bd7d7227d0
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-18 04:59:34 +0000

    * Cats hunting rats.
    Former-commit-id: a76654649c7da6653a12310a9b653dae1ad8c1b7

--- kmud-live/scripts/flavor/cats/19509_CatEmotes.js
+++ kmud-live/scripts/flavor/cats/19509_CatEmotes.js
@@ -1,17 +1,53 @@
 var script19509 = function(self, actor, here, args, extra) {
 
+	if(self.position != constants.POS_STANDING)
+		return;
 	//Give priority to killing rats.
 	//Galnor 2014-07-18
+	var svalName = "rat";
+	var svalVnum = 19509;
 
-	var rats = here.getCharacters(function(character) {
+	var ratHunting = getSval(self, svalVnum, svalName);
 
-		return character.vnum == 79;
-	});
+	if(ratHunting && ratHunting.room.vnum != self.room.vnum)
+	{//Try to chase the rat...
 
-	if(rats.length != 0)
-	{
-		var rat = rats[0];
+		if(ratHunting.isValid && ratHunting.room.zoneVnum == here.zoneVnum)
+		{
+			var direction = here.firstStep(ratHunting.room);
+			if(direction >= 0)
+			{
+				act("$n sniffs at something on the ground...", true, self, null, null, constants.TO_ROOM);
 
+				wait 1;
+				self.comm(dirToText(direction));
+				return;
+			}
+		}
+
+		//We can't get here if the cat chased. If we get here the rat could not be tracked. Forget about it.
+		setSval(self, svalVnum, svalName, null);
+		ratHunting = null;
+	}
+
+	var rat = null;
+
+	//If we are in the room of the rat we are hunting, attack it.
+	if(ratHunting != null && ratHunting.isValid && ratHunting.room.vnum == self.room.vnum)
+		rat = ratHunting;
+	else
+	{//Otherwise, try to find another one.
+		var rats = here.getCharacters(function(character) {
+
+			return character.vnum == 79;
+		});
+
+		if(rats.length != 0)
+			rat = rats[0];
+	}
+
+	if(rats)
+	{//If there is a rat in the room, attack it!
 		act("$n sniffs at something on the ground...", true, self, null, null, constants.TO_ROOM);
 
 		wait 2;
@@ -36,4 +72,4 @@ var script19509 = function(self, actor, here, args, extra) {
 	        target.send(self.name + " growls at you.");
 	    }
 	}
-}
+};
