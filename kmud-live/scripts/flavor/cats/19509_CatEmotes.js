32dcebdaab42c527e64ec7c9c2ae11f3dbe62841
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-18 16:10:14 +0000

    Cat room emotes now have the first letter of the cat's name capitalized
    Former-commit-id: 0f63de81a2e8c53665d6ad3c6c838f1793dbcf66

--- kmud-live/scripts/flavor/cats/19509_CatEmotes.js
+++ kmud-live/scripts/flavor/cats/19509_CatEmotes.js
@@ -62,16 +62,16 @@ var script19509 = function(self, actor, here, args, extra) {
 
 	var rand = random(1,16);
 	if( rand == 1 )
-	    here.echo(self.name + " sniffs at something on the ground...");
+	    here.echo(capFirstLetter(self.name) + " sniffs at something on the ground...");
 	else if( rand == 2 )
-	    here.echo(self.name + " purrs softly.");
+	    here.echo(capFirstLetter(self.name) + " purrs softly.");
 	else if( rand == 3 )
 	{
 	    target = getRandomPersonInRoom(here);
 	    if( target != self && self.canSee(target) )
 	    {
-	        here.echoaround(target, self.name + " growls at " + target.name + ".");
-	        target.send(self.name + " growls at you.");
+	        here.echoaround(target, capFirstLetter(self.name) + " growls at " + target.name + ".");
+	        target.send(capFirstLetter(self.name) + " growls at you.");
 	    }
 	}
 };
