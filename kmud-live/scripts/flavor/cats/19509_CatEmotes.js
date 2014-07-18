10590e7daf8aeb2ae2362296bb3ddd58011d0591
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-18 04:40:34 +0000

    * Cats hunting rats.
    Former-commit-id: a483f265823b648971e1a51723a40c06cfff8b42

--- kmud-live/scripts/flavor/cats/19509_CatEmotes.js
+++ kmud-live/scripts/flavor/cats/19509_CatEmotes.js
@@ -17,7 +17,7 @@ var script19509 = function(self, actor, here, args, extra) {
 		wait 2;
 
 		if(rat.isValid && rat.room.vnum == self.room.vnum)
-			self.hit(rat);
+			self.setFighting(rat);
 
 		return;
 	}
