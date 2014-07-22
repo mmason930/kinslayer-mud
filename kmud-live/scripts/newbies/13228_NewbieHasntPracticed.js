820142c799dccff4dc892e729942b3d449df6bd8
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-22 02:01:15 +0000

    * Newbie - Has Not Practiced message
    Former-commit-id: fc28c6ef8a8d230db2527b1e760f52de0ed7fcf9

--- kmud-live/scripts/newbies/13228_NewbieHasntPracticed.js
+++ kmud-live/scripts/newbies/13228_NewbieHasntPracticed.js
@@ -6,7 +6,7 @@ var script13228 = function(self, actor, here, args, extra) {
 			13233: constants.SOUTH,
 			13247: constants.WEST,
 			13338: constants.EAST
-		}
+		};
 
 		var directionRequired = directionMap[here.vnum];
 
@@ -17,8 +17,10 @@ var script13228 = function(self, actor, here, args, extra) {
 			return;
 
 		getCharCols(actor);
+		
+		waitpulse 1;
 		actor.send(bld + red + "You are leaving the main area of the village without fully training your skills." + nrm);
-		actor.send(bld + red + "Return " + bld + cyn + dirToText(revDir(extra.direction)).toUpperCase() + bld + red + " to practice your skills." + nrm);
+		actor.send(bld + red + "Return " + bld + cyn + dirToText(extra.direction).toUpperCase() + bld + red + " to practice your skills." + nrm);
 		actor.send(bld + red + "If you have trouble finding the trainers, be sure to " + bld + cyn + "READ MAP" + nrm + ".");
 	}
 };
