4a9f99eddf94b892010900426e2e9bb55590ab0a
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-22 01:23:51 +0000

    * Smoke bomb message.
    Former-commit-id: 4dd9b61ba4617b5b1f3b9aed5650671c56cf7fc3

--- kmud-live/scripts/scripts/19569_SmokeBomb.js
+++ kmud-live/scripts/scripts/19569_SmokeBomb.js
@@ -1,9 +1,8 @@
 var script19569 = function(self, actor, here, args, extra) {
-	here.echo("Test");
 	waitpulse 4;
 	actor.send("You vanish in a cloud of smoke!");
 	actor.moveToRoom(getRoom(1700));
 	actor.comm("look");
 	here.echo("A cloud of smoke fills the room!");
 	self.extract();
-};//////
+};
