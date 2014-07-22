9c37a24f2101658a3c690989968da6b6a7bd9473
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-22 02:00:25 +0000

    * Newbie - Has Not Practiced message
    Former-commit-id: 95ee85c6771a89c9877299e76e529241ac444620

--- kmud-live/scripts/newbies/13228_NewbieHasntPracticed.js
+++ kmud-live/scripts/newbies/13228_NewbieHasntPracticed.js
@@ -1,11 +1,24 @@
 var script13228 = function(self, actor, here, args, extra) {
 
-	here.echo("Test");
 	if(actor.level == 1 && actor.skillPractices >= 6)
 	{
-		here.echo("Dir: " + extra.dir);
+		var directionMap = {
+			13233: constants.SOUTH,
+			13247: constants.WEST,
+			13338: constants.EAST
+		}
+
+		var directionRequired = directionMap[here.vnum];
+
+		if(directionRequired == null)
+			mudLog(constants.BRF, Math.MAX(actor.invis, 100), "Player `" + actor.name + "` entered room not found in map. Newbie Not Practiced Script.");
+
+		if(directionRequired != extra.direction)
+			return;
+
 		getCharCols(actor);
 		actor.send(bld + red + "You are leaving the main area of the village without fully training your skills." + nrm);
-//		actor.send(bld + red + "Return ")
+		actor.send(bld + red + "Return " + bld + cyn + dirToText(revDir(extra.direction)).toUpperCase() + bld + red + " to practice your skills." + nrm);
+		actor.send(bld + red + "If you have trouble finding the trainers, be sure to " + bld + cyn + "READ MAP" + nrm + ".");
 	}
-};//
+};
