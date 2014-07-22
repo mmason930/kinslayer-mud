227e23d071e36234230321a0d742d6f986140195
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-22 03:14:13 +0000

    * Newbie - Has Not Practiced message
    Former-commit-id: ac3d9efaed56b514349c2867a10383eb2f5fbfcd

--- kmud-live/scripts/scripts/13224_TrainerEmoteUponEnteringRoom.js
+++ kmud-live/scripts/scripts/13224_TrainerEmoteUponEnteringRoom.js
@@ -2,17 +2,87 @@ var script13224 = function(self, actor, here, args, extra) {
 	waitpulse 8;
 	var className = classNumToText(self.class);
 	var heShe = self.sex == constants.SEX_MALE ? "he" : "she";
+	var practices;
 	var exampleSkillName = "RIDE";
-	if(self.class == constants.CLASS_RANGER)
+	if(self.class == constants.CLASS_RANGER) {
 		exampleSkillName = "TRACK";
-	else if(self.class == constants.CLASS_THIEF)
+		practices = actor.skillPractices;
+	}
+	else if(self.class == constants.CLASS_THIEF) {
 		exampleSkillName = "SHORT BLADES";
-	else if(self.class == constants.CLASS_CHANNELER)
+		practices = actor.skillPractices;
+	}
+	else if(self.class == constants.CLASS_CHANNELER) {
 		exampleSkillName = "EARTH";
+		practices = actor.spellPractices;
+	}
 	getCharCols(actor);
 	if(self.class != constants.CLASS_CHANNELER || (actor.class == constants.CLASS_CHANNELER && actor.sex == self.sex)) {
 		actor.send( "The " + bld + red + className + nrm + " trainer is in this room. You can use the " + cyn + "PRACTICE" + nrm + " command to see a list of skills\n"
 				+	"that " + heShe + " can teach you. To practice a skill, simply type \"practice\" followed by the skill name.\n"
 				+	"For example: " + cyn + "PRACTICE " + exampleSkillName + nrm + "\n");
 	}
-}
+
+	if(actor.level <= 15) {
+		wait 2;
+
+		if (actor.room.vnum != self.room.vnum || !self.canSee(actor))
+			return;
+
+		if (practices == 0) {
+			self.say("You are fully trained here at this time, " + actor.name + ".");
+			return;
+		}
+
+		if (self.class == constants.CLASS_THIEF && actor.class == constants.CLASS_THIEF && actor.getSkill(constants.SKILL_SHORT_BLADE) == 0) {
+
+			self.say("You are a thief, " + actor.name + ", one of my kind. I recommend you " + bld + cyn + "PRACTICE SHORT BLADES" + nrm);
+		}
+		else if(self.class == constants.CLASS_WARRIOR)
+		{
+			if(actor.class == constants.CLASS_THIEf)
+				self.say("You are a thief, " + actor.name + ". While you may want to learn some of my skills, you should really go see a thief trainer. They will know how to train your kind!");
+			else
+			{
+
+				var weaponTypeToData = {};
+				weaponTypeToData[constants.WEAPON_AXE] = {name: "axe", skillName: "axes"};
+				weaponTypeToData[constants.WEAPON_LONG_BLADE] = {name: "long blade", skillName: "long blades"};
+				weaponTypeToData[constants.WEAPON_SHORT_BLADE] = {name: "short blade", skillName: "short blades"};
+				weaponTypeToData[constants.WEAPON_CLUB] = {name: "club", skillName: "clubs"};
+				weaponTypeToData[constants.WEAPON_STAFF] = {name: "staff", skillName: "staves"};
+				weaponTypeToData[constants.WEAPON_SPEAR] = {name: "spear", skillName: "spears"};
+				weaponTypeToData[constants.WEAPON_LANCE] = {name: "lance", skillName: "lances"};
+				weaponTypeToData[constants.WEAPON_CHAIN] = {name: "chain", skillName: "chains"};
+				weaponTypeToData[constants.WEAPON_LANCE] = {name: "lance", skillName: "lances"};
+
+				//Figure out which weapon they are most likely to use.
+				var weaponWielded = actor.eq(constants.WEAR_WIELD);
+				var weapons = actor.inventory.filter(function(item) {
+					return item.type == constants.ITEM_WEAPON;
+				});
+
+				if(weapon)
+				{
+					if(weapon.value(0) == constants.WEAPON_SHORT_BLADE) {
+						self.say("I see that you are wielding a short blade, " + actor.name + ". Short blades are a thief's tool. You should use another weapon instead!");
+						return;
+					}
+					else
+					{
+						var weaponData = weaponTypeToData[ weapon.value(0) ];
+
+						if(!weaponData)
+						{
+							mudLog(constants.BRF, 100, "Unknown weapon type `" + weapon.value(0) + "` for item #" + weapon.value + ". script13224.");
+							return;
+						}
+
+						self.say("I see you are wielding a " + weaponData.name + ". You can train in that weapon here by entering " + bld + cyn + "PRACTICE " + weaponData.skillName.toUpperCase() + nrm);
+					}
+				}
+			}
+		}
+		self.say("")
+	}
+};
