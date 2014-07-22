4fac130870c02abb93e9ea4cccc25cb3c3c9ba20
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-22 01:43:21 +0000

    * Newbies - Did Not Practice Message
    Former-commit-id: 95c54fc0126483bf4c1c2506e1aa469ab3d121b6

---
+++ kmud-live/scripts/newbies/13228_NewbieHasntPracticed.js
@@ -0,0 +1,10 @@
+var script16 = function(self, actor, here, args, extra) {
+
+	if(actor.level == 1 && actor.getSkillPractices() >= 6)
+	{
+		here.echo("Dir: " + extra.dir);
+		getCharCols(actor);
+		actor.send(bld + red + "You are leaving the main area of the village without fully training your skills." + nrm);
+//		actor.send(bld + red + "Return ")
+	}
+};
