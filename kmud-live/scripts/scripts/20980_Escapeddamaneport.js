c8cbddf32fff1612fd3f4648abae0f909864cfc4
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-24 04:45:53 +0000

    20980
    Former-commit-id: 35e988c398ff0b8b85cd8316c4e4b9acd4aac0ac

--- kmud-live/scripts/scripts/20980_Escapeddamaneport.js
+++ kmud-live/scripts/scripts/20980_Escapeddamaneport.js
@@ -1,8 +1,8 @@
 var script20980 = function(self, actor, here, args, extra) {
 	var newRoom = getRandomRoom();
 	if(newRoom){
-		act(self.name+" takes a last look around before disappearing in a flash of bright light.",true,actor,null,null,constants.TO_ROOM);
+		act(self.name+" takes a last look around before disappearing in a flash of bright light.",true,self,null,null,constants.TO_ROOM);
 		self.moveToRoom(newRoom);
-		act(self.name+" arrives in a flash of bright light!",true,actor,null,null,constants.TO_ROOM);
+		act(self.name+" arrives in a flash of bright light!",true,self,null,null,constants.TO_ROOM);
 	}
 }
