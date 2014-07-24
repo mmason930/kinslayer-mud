800950051510aa67fd43dc711a2267e3e88277dc
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-24 18:19:19 +0000

    Former-commit-id: a2c8694bd01a5efd66ac334cc3a408c1a5a3514c

--- kmud-live/scripts/scripts/20980_Escapeddamaneport.js
+++ kmud-live/scripts/scripts/20980_Escapeddamaneport.js
@@ -1,14 +1,16 @@
 var script20980 = function(self, actor, here, args, extra) {
 	var roomArr = [81, 10, 333, 80, 85, 20, 332, 18, 66, 181, 206, 193, 64, 17, 93, 19, 334, 103, 70, 100, 3, 1, 70, 214, 217, 4, 31, 201, 9, 230, 235, 100, 186, 53, 101, 102, 45, 134, 132];
-	var newRoom = roomArr[random(0,roomArr.length-1)];
+	var newRoom = getRandomRoom();
 	if(newRoom && !self.isBashed){
-		act(self.name+" disappears in a flash of bright light.",true,self,null,null,constants.TO_ROOM);
-		self.moveToRoom(newRoom);
-		act(self.name+" arrives in a flash of bright light!",true,self,null,null,constants.TO_ROOM);
-		var aMob = getMobAtRoom(20800, 20804);
-		if(aMob){
-			setSval(aMob, 20980, self.vnum, newRoom.zoneVnum);
-			//sendKoradin(getSval(aMob, 20980, self.vnum));
+		if(arrContains(roomArr, newRoom.zoneVnum)){
+			act(self.name+" disappears in a flash of bright light.",true,self,null,null,constants.TO_ROOM);
+			self.moveToRoom(newRoom);
+			act(self.name+" arrives in a flash of bright light!",true,self,null,null,constants.TO_ROOM);
+			var aMob = getMobAtRoom(20800, 20804);
+			if(aMob){
+				setSval(aMob, 20980, self.vnum, newRoom.zoneVnum);
+				//sendKoradin(getSval(aMob, 20980, self.vnum));
+			}
 		}
 	}
 }
