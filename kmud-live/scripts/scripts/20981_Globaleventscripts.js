059130a445803eb2faa747e0a8642031b9323219
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-28 01:59:05 +0000

    Former-commit-id: 0fdb7dcb2c48686a021b59833075d9445f6e221d

---
+++ kmud-live/scripts/scripts/20981_Globaleventscripts.js
@@ -0,0 +1,7 @@
+var script20981 = function(self, actor, here, args, extra) { // murash/agelmar enter script
+	if(global.eventIsActive){
+		waitpulse 1;
+		getCharCols(actor);
+		actor.send(cyn+bld+self.name+" has a special assignment! Say JOIN EVENT to get started."+nrm);
+	}
+}
