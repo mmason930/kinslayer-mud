4a8f1e29892be2e7969146bfaa773121d7bec735
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-28 02:30:01 +0000

    Former-commit-id: 2c38c73fd7b061a73315a02c1cfd8f4edc4d60d0

--- kmud-live/scripts/scripts/20981_Globaleventscripts.js
+++ kmud-live/scripts/scripts/20981_Globaleventscripts.js
@@ -1,5 +1,6 @@
 var script20981 = function(self, actor, here, args, extra) { // murash/agelmar enter script
 	if(global.eventIsActive && actor.race == self.race){
+		sendKoradin("working");
 		waitpulse 1;
 		getCharCols(actor);
 		actor.send(cyn+bld+self.name+" has a special assignment! Type JOIN EVENT to get started."+nrm);
