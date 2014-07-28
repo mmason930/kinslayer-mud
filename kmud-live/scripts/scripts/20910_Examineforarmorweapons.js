3ca9b0131f30586979994ec40de93d27c447d107
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-28 04:43:19 +0000

    Former-commit-id: 905194393920e3b4b1456aefb0ff2976fdc98969

--- kmud-live/scripts/scripts/20910_Examineforarmorweapons.js
+++ kmud-live/scripts/scripts/20910_Examineforarmorweapons.js
@@ -67,7 +67,7 @@ var script20910 = function(self, actor, here, args, extra) {
 		return;
 	}
 	if(global.Global2014Util){ // examining pedestals for global event
-		var pedestal = global.global2014Util.objectIdToPedestalMap[ obj.id ];
+		var pedestal = global.Global2014Util.objectIdToPedestalMap[ obj.id ];
 		if(pedestal){
 			actor.send(" ");
 			actor.send("This pedestal currently has "+pedestal.getHitPoints()+" out of "+pedestal.getMaxHitPoints()+" hit points.");
