cd13020120a726f683e4881e556f1e561fe2ac4c
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-28 03:36:56 +0000

    * Global Quest July 31st, 2014
    Former-commit-id: 6f50b70d568d58536e5e163e5cbeac74d604c7c4

--- kmud-live/scripts/quests/global-quest-2014-07-31/Global2014Pedestal.js
+++ kmud-live/scripts/quests/global-quest-2014-07-31/Global2014Pedestal.js
@@ -4,6 +4,8 @@ function Global2014Pedestal(object, maxHitPoints, race)
 	this.maxHitPoints = maxHitPoints;
 	this.hitPoints = maxHitPoints;
 	this.race = race;
+	this.roomVnum = object.room.vnum;
+	this.objectId = object.id;
 }
 
 Global2014Pedestal.getObject = function()
@@ -24,4 +26,37 @@ Global2014Pedestal.getHitPoints = function()
 Global2014Pedestal.getRace = function()
 {
 	return this.race;
+};
+
+Global2014Pedestal.takeDamage = function(character, damageAmount)
+{
+	if(!this.object.isValid)
+	{
+		mudLog(constants.BRF, 100, "Pedestal `" + this.objectId + "` in room #" + this.roomVnum + " is invalid but taking damage(" + damageAmount + ").");
+		return;
+	}
+
+	if(!character.isValid)
+	{
+		mudLog(constants.BRF, 100, "Pedestal `" + this.objectId + "` in room #" + this.roomVnum + " is taking damage(" + damageAmount + ") from invalid character.");
+		return;
+	}
+
+	if(damageAmount <= 0)
+	{
+		mudLog(constants.BRF, 100, "Pedestal `" + this.objectId + "` in room #" + this.roomVnum + " is taking negative damage(" + damageAmount + ") from " + character.name + ".");
+		return;
+	}
+
+	mudLog(constants.BRF, 100, "Pedestal in room #" + this.roomVnum + " has taken " + damageAmount + " from " + character.name + ".");
+
+	this.hitPoints = Math.max(0, this.getHitPoints() - damageAmount);
+
+	mudLog(constants.BRF, 100, "Pedestal in room #" + this.roomVnum + " is now at " + this.getHitPoints() + " hit point" + (this.getHitPoints() == 1 ? "" : "s") + ".");
+
+	if(this.hitPoints <= 0)
+	{
+		mudLog(constants.BRF, 100, "Pedestal in room #" + this.roomVnum + " has been destroyed.");
+		this.getObject().extract();
+	}
 };
