711d9c7b7b34c441d7bc27cbf18bb292b8f86723
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-29 03:05:11 +0000

    Former-commit-id: 44b8af45bd6aec9986cdf2b1205e972f9d41444a

--- kmud-live/scripts/scripts/20860_Death.js
+++ kmud-live/scripts/scripts/20860_Death.js
@@ -16,7 +16,7 @@ var script20860 = function(self, actor, here, args, extra) {
 	
 	// Damane global event July 2014
 	if(global.global2014Util){
-		if(arrContains(global.global2014util.damaneVnums, self.vnum)){
+		if(arrContains(global.global2014Util.damaneVnums, self.vnum)){
 			mudLog(2, 100, self.name+" has been killed by "+actor.name);
 			global.global2014Util.damaneZoneVnums[self.vnum] = 0;
 		}
