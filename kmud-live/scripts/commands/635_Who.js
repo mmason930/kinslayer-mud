8a19d8fb11a7f718e41d05a5a11369c1c73005b8
koradin <koradin@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-07-29 16:48:27 +0000

    Former-commit-id: 920bb62f58ff0ce0b78b8aca80677f887c69df10

--- kmud-live/scripts/commands/635_Who.js
+++ kmud-live/scripts/commands/635_Who.js
@@ -330,7 +330,7 @@ var script635 = function(self, actor, here, args, extra) {
 			}
 			if(global.global2014Util){ // global event
 				if(global.global2014Util.eventIsActive){
-					disp += cyn+bld+"\n\nThe Kinslayer global event is currently running!\nLight Points: "+global.global2014Util.pointMap[constants.RACE_HUMAN]+"  Dark Points: "+global.global2014Util.pointMap[constants.RACE_TROLLOC]+nrm;
+					disp += cyn+bld+"\n\nThe Kinslayer global event is active! Type JOIN EVENT to enter.\nLight Points: "+global.global2014Util.pointMap[constants.RACE_HUMAN]+"  Dark Points: "+global.global2014Util.pointMap[constants.RACE_TROLLOC]+nrm;
 				}
 			}
 		}
