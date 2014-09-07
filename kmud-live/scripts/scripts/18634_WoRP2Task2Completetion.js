d85026dc44866526c87053e5c0ad05fe7e695690
rhollor <rhollor@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-09-07 04:55:31 +0000

    Former-commit-id: a16dcfe2cf4c8110270cb15e6eeb38a9d5813a40

--- kmud-live/scripts/scripts/18634_WoRP2Task2Completetion.js
+++ kmud-live/scripts/scripts/18634_WoRP2Task2Completetion.js
@@ -8,7 +8,7 @@ var script18634 = function(self, actor, here, args, extra) {
 		wait 1;
 		self.comm("say I can see that you have a desire to learn and understand what you have stumbled upon.");
 		waitpulse 14;
-		self.comm("say If you trully wish to learn about the Way of Ruin, you must complete tasks to prove yourself to Us.");
+		self.comm("say If you truly wish to learn about the Way of Ruin, you must complete tasks to prove yourself to Us.");
 		waitpulse 7;
 		actor.updateJournalTask(qName,1);
 		//actor.send("You have unlocked quest(s) from: " + bld + cyn + self.name + nrm + ".");
