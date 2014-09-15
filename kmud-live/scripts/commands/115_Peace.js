3ca96cacc01a836d2579a6677881b610a547d6eb
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-09-15 21:53:56 +0000

    * Fix peace command.
    Former-commit-id: 41500b0bf3d51e36a1288b44e15e028557442acf

--- kmud-live/scripts/commands/115_Peace.js
+++ kmud-live/scripts/commands/115_Peace.js
@@ -11,17 +11,16 @@ var script115 = function(self, actor, here, args, extra) {
 		actor.send("You cannot use that command.");
 		return;
 	}
-	var vict;
 	here.echo("Everything is quite peaceful now.");
 	var people = here.people;
 	for(var index = 0;index < people.length;++index) {
 		
 		var vict = people[index];
-		if(vict.level >= actor.level) {
+
+		if(vict.level > actor.level) {
 			continue;
 		}
 		vict.stopFighting();
 		vict.position = constants.POS_SITTING;
 	}
-	here.echo("Test");
-}
+};
