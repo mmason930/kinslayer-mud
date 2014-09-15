90f14ff0b842b154b2a2326e3e47fa2caa75bbaa
rhollor <rhollor@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-09-15 21:33:59 +0000

    Former-commit-id: 2995d1d57444b1da3f92bb3cf30c2e706c02931b

--- kmud-live/scripts/flavor/rats-ravens-crows/21010_RavenCrowLand.js
+++ kmud-live/scripts/flavor/rats-ravens-crows/21010_RavenCrowLand.js
@@ -1,4 +1,4 @@
-var ravenCrowLand = function(self, actor, here, args, extra) {
+var ravencrowland = function(self, actor, here, args, extra) {
 	if(self.position == constants.POS_FLYING) {
 		self.comm("emote starts to slowly descend from the sky and lands, eyes flashing in your direction.");
 		self.position = constants.POS_STANDING;
