bdb47c6487a7b3e4298acf3a37d1d0cc4d514b07
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-11-09 15:34:46 +0000

    * Typo
    Former-commit-id: 94a2e7c2055e14706ee55a6f6d5534b9a630311e

--- kmud-live/scripts/scripts/1008_QUESTfoodpickupforFeedingLowCaemlynsPoor.js
+++ kmud-live/scripts/scripts/1008_QUESTfoodpickupforFeedingLowCaemlynsPoor.js
@@ -2,7 +2,7 @@ var script1008 = function(self, actor, here, args, extra) {
 	var qName = "Feeding Low Caemlyn's Poor";
 	if ( actor.quest(qName) > 0 && !actor.questTaskProgress(qName)[1].comp ) {
 		waitpulse 1;
-		self.tell(actor, "Thank the light you're here! Please, take these packages and distribute them to the poor right away!");
+		self.tell(actor, "Thank the Light you're here! Please, take these packages and distribute them to the poor right away!");
 		self.loadObj(10000);
 		self.loadObj(10000);
 		self.loadObj(10000);
