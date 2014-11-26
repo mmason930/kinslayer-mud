ae9e4bf1f4c31789e2b662c5e18df614a6dc82c9
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-11-26 18:53:01 +0000

    * From Alder - Fix for Previous Metals quest.
    Former-commit-id: f3cee581adb000637a9bc6b9d95f44a83edadd62

--- kmud-live/scripts/scripts/9886_PreciousMetalsTakeTaskItem.js
+++ kmud-live/scripts/scripts/9886_PreciousMetalsTakeTaskItem.js
@@ -1,14 +1,13 @@
 var script9886 = function(self, actor, here, args, extra) {
 	//Alder
 	//May 2010
-	var i;
-	if ( self.name == "a chunk of gold ore" ) {
+	var i = -1;
+	if (self.name.match(/a (large )*chunk of gold ore/))
 		i = 0;
-	}
-	else if ( self.name == "a chunk of platinum ore" ) {
+	else if (self.name.match(/a (large )*chunk of platinum ore/))
 		i = 1;
-	}
-	else if ( self.vnum == 1002 )
+	else if (self.vnum == 1002)
 		i = 2;
-	actor.updateJournalTask('Precious Metals',i);
-}
+	if (i != -1)
+		actor.updateJournalTask('Precious Metals',i);
+};
