2743a2788f034c909919b2650a4fa5d5fc3655df
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-03 15:31:06 +0000

    Former-commit-id: 5ee4bd10c42421ba400ba44e076a27642387a5d1

--- kmud-live/scripts/scripts/20835_stonesdrop-Koradin.js
+++ kmud-live/scripts/scripts/20835_stonesdrop-Koradin.js
@@ -50,7 +50,7 @@ var script20835 = function(self, actor, here, args, extra) {
         }
     }
 	if (trigger == false) {
-        return;
+        return false;
     }
     room.echo(msg1);
     setTimeout(30, echoMsg, [room,msg2]));
