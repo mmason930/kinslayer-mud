bd3034e984c1198f556d79f9f43e64143bfb7b73
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-08 21:23:03 +0000

    Former-commit-id: f7d088a543dc78bbbfa071e48f73bfe49497b5e2

--- kmud-live/scripts/scripts/addMissingRaresToAuction.js
+++ kmud-live/scripts/scripts/addMissingRaresToAuction.js
@@ -1,12 +1,12 @@
 var addMissingRaresToAuction =  function(self, actor, here, args, extra) {
     var holder = null;
+    here.echo(self.name);return;
     for each (var person in here.people) {
         if (person.namelist.split(" ")[2] == 'fogeltest') {
             holder = person;
-            here.echo(holder.name);
         }
     }
-    return;
+
     var date = new Date();
     // Run every Thursday
     if (date.getDay() != 4) {
@@ -26,7 +26,7 @@ var addMissingRaresToAuction =  function(self, actor, here, args, extra) {
         
         var obj = null;
         obj = loadSingleObjectFromDatabase(row.get('object_id'));
-        obj.moveToChar(actor);
+        obj.moveToChar(holder);
         if (row.get('top_level_holder_type') == 'P') {
             mudLog(2, 104, "Inactive item extracted from user " + row.get('username') + " with last logon time of " + row.get('last_logon') + " : vnum - " + row.get('vnum') + " sdesc - " + row.get('sdesc') + " id - " + row.get('object_id'));
         } else {
