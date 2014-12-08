dbe13712414dbb8d94599f883976a0bf0c4b0fdb
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-08 22:01:47 +0000

    Former-commit-id: 808d864d0173b56e9ed15b296407f1b96997a8ed

--- kmud-live/scripts/scripts/addMissingRaresToAuction.js
+++ kmud-live/scripts/scripts/addMissingRaresToAuction.js
@@ -4,37 +4,6 @@ var addMissingRaresToAuction =  function(self, actor, here, args, extra) {
     if (date.getDay() != 4) {
         return;
     }
-    // Purge all inactive RARE items
-    var query = '';
-    query = "SELECT u.username, u.last_logon, o.vnum, o.id AS object_id, op.sdesc, o.top_level_holder_type FROM objects o "
-                + "LEFT JOIN users u ON u.user_id = o.top_level_holder_id "
-                + "LEFT JOIN obj_protos op ON o.vnum = op.vnum "
-                + "WHERE op.extra_flags & ( 1 <<15 ) "
-                + "AND (u.last_logon <= DATE_SUB( CURDATE( ) , INTERVAL 30 DAY )  OR o.top_level_holder_type = 'O')";
-    var result = sqlQuery(query);
-    
-    while( result.hasNextRow ) {
-        var row = result.getRow;
-        
-        var obj = null;
-        obj = loadSingleObjectFromDatabase(row.get('object_id'));
-        obj.moveToChar(self);
-        if (row.get('top_level_holder_type') == 'P') {
-            mudLog(2, 104, "Inactive item extracted from user " + row.get('username') + " with last logon time of " + row.get('last_logon') + " : vnum - " + row.get('vnum') + " sdesc - " + row.get('sdesc') + " id - " + row.get('object_id'));
-        } else {
-            mudLog(2, 104, "Inactive item extracted, is not on user: vnum - " + row.get('vnum') + " sdesc - " + row.get('sdesc') + " id - " + row.get('object_id'));        
-        }
-    }
-
-    // Get list of all RARE items
-    var sql = "SELECT vnum FROM obj_protos WHERE extra_flags & ( 1 <<15 )";
-    var result = sqlQuery(sql);
-    
-    var rareItems = [];
-    while (result.hasNextRow) {
-        var row = result.getRow;
-        rareItems.push(row.get("vnum"));
-    }
     
     var auctionableRareItems = [
         1611,   // a platinum mace with steel spikes
@@ -53,6 +22,41 @@ var addMissingRaresToAuction =  function(self, actor, here, args, extra) {
         21799,  // a Sharan-emblazoned cuirass
         1416    // a sharp dagger of obsidian
     ];
+    
+    // Purge all inactive RARE items
+    var query = '';
+    query = "SELECT u.username, u.last_logon, o.vnum, o.id AS object_id, op.sdesc, o.top_level_holder_type FROM objects o "
+                + "LEFT JOIN users u ON u.user_id = o.top_level_holder_id "
+                + "LEFT JOIN obj_protos op ON o.vnum = op.vnum "
+                + "WHERE op.extra_flags & ( 1 <<15 ) "
+                + "AND (u.last_logon <= DATE_SUB( CURDATE( ) , INTERVAL 30 DAY )  OR o.top_level_holder_type = 'O')";
+    var result = sqlQuery(query);
+    
+    while( result.hasNextRow ) {
+        var row = result.getRow;
+        
+        var obj = null;
+        obj = loadSingleObjectFromDatabase(row.get('object_id'));
+        obj.moveToChar(self);
+        if (auctionableRareItems.indexOf(obj.vnum) == -1) {
+            self.comm("drop "+obj.namelist.split(" ")[0]);
+        }
+        if (row.get('top_level_holder_type') == 'P') {
+            mudLog(2, 104, "Inactive item extracted from user " + row.get('username') + " with last logon time of " + row.get('last_logon') + " : vnum - " + row.get('vnum') + " sdesc - " + row.get('sdesc') + " id - " + row.get('object_id'));
+        } else {
+            mudLog(2, 104, "Inactive item extracted, is not on user: vnum - " + row.get('vnum') + " sdesc - " + row.get('sdesc') + " id - " + row.get('object_id'));        
+        }
+    }
+
+    // Get list of all RARE items
+    var sql = "SELECT vnum FROM obj_protos WHERE extra_flags & ( 1 <<15 )";
+    var result = sqlQuery(sql);
+    
+    var rareItems = [];
+    while (result.hasNextRow) {
+        var row = result.getRow;
+        rareItems.push(row.get("vnum"));
+    }
     
     // Load items for auction
     for each (var vnum in auctionableRareItems) {
