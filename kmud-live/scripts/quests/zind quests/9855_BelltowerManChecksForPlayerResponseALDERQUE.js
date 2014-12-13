04f3578fbe3677056213890db0081c3b53feaddd
alder <alder@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-13 05:46:53 +0000

    Fixing Just Think...
    Former-commit-id: eacc417cc54e4c7b99674fcd2a0e097667f3bccc

--- kmud-live/scripts/quests/zind quests/9855_BelltowerManChecksForPlayerResponseALDERQUE.js
+++ kmud-live/scripts/quests/zind quests/9855_BelltowerManChecksForPlayerResponseALDERQUE.js
@@ -3,9 +3,10 @@ var script9855 = function(self, actor, here, args, extra) {
 	// here.echo("Wait sval: "+getSval(self,9855,"Wait"));
 	if ( getSval(self,9855,"Wait") == 2 ) {
 		// Don't unlock the next quest if they don't respond
+		actor = getSval(self,9854,"Actor");
+
 		actor.qval("ZIND_UNLOCK_SECOND_QUEST", 0);
 
-		actor = getSval(self,9854,"Actor");
 		getCharCols(actor,constants.CL_OFF);
 		actor.send(capFirstLetter(self.name)+" cuts loose a small pouch and disappears over the balcony railing before the clinking of coin is heard as the pouch hits the ground.");
 		self.moveToRoom(getRoom(501));
