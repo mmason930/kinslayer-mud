6f5afed604b884d7b078268d01ab7907e10773b7
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-16 19:30:22 +0000

    Former-commit-id: 1adba64da28b143b4bd0ffacb7053c9883961a97

--- kmud-live/scripts/scripts/20837_forestofvinesgreenstonespread-Koradin.js
+++ kmud-live/scripts/scripts/20837_forestofvinesgreenstonespread-Koradin.js
@@ -64,7 +64,7 @@ var script20837 = function(self, actor, here, args, extra) {
             }
         }
         var times_run = getSval(room, 20837, 'greenStoneTime');
-        times_run = (times_run == 'undefined' || times_run == 'NaN') ? 0 : times_run;
+        times_run = typeof(times_run) != 'number' ? 0 : times_run;
         here.echo(times_run);
         if (times_run > 5 && (times_run >= 10 || random(1, 100) <= 5)) {
             for (var _autoKey in room.people) {
