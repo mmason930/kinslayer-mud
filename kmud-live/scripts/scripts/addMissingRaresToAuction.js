f938df7381872ba1780874c1d38bc1b4bcd115b3
fogel <fogel@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-19 22:14:41 +0000

    Former-commit-id: eb5c2441515f7043bac489c2e80461487734a3f9

--- kmud-live/scripts/scripts/addMissingRaresToAuction.js
+++ kmud-live/scripts/scripts/addMissingRaresToAuction.js
@@ -29,7 +29,7 @@ var addMissingRaresToAuction =  function(self, actor, here, args, extra) {
                 + "LEFT JOIN users u ON u.user_id = o.top_level_holder_id "
                 + "LEFT JOIN obj_protos op ON o.vnum = op.vnum "
                 + "WHERE op.extra_flags & ( 1 <<15 ) "
-                + "AND (u.last_logon <= DATE_SUB( CURDATE( ) , INTERVAL 30 DAY )  OR o.top_level_holder_type = 'O')";
+                + "AND (u.last_logon <= DATE_SUB( CURDATE( ) , INTERVAL 30 DAY )  OR o.top_level_holder_type = 'O' OR o.top_level_holder_type = 'C')";
     var result = sqlQuery(query);
     
     while( result.hasNextRow ) {
