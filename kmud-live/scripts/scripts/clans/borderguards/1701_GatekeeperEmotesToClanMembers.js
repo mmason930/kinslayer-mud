e3acd5db877411093c8d2a246aaba90110d09e2d
mmason <mmason@5192608c-848b-b846-b5d7-abe55ab34c4a>
2014-12-22 08:25:08 +0000

    * Global quest, December 2014.
    Former-commit-id: b4c4a20856aee6745e22fd0cd36ddbac28a5c8d6

--- kmud-live/scripts/scripts/clans/borderguards/1701_GatekeeperEmotesToClanMembers.js
+++ kmud-live/scripts/scripts/clans/borderguards/1701_GatekeeperEmotesToClanMembers.js
@@ -1,4 +1,23 @@
 var script1701 = function(self, actor, here, args, extra) {
 
-	here.echo("test");
+	if(!actor.inClan(3) || self.position <= constants.POS_FIGHTING || !self.canSee(actor))
+		return;
+
+	waitpulse 2;
+
+	switch(random(1, 5))
+	{
+		case 1:
+			self.comm("nod " + actor.name);
+			break;
+		case 2:
+			self.say("Peace favor your sword, " + actor.name + ".");
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+	}
 };
