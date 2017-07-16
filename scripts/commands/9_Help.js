ddbfcd9fc3fa94ce995e4dc72a0f39d07bac499a
Michael Mason <mikemason930@gmail.com>
2017-07-15 22:15:58 -0400

    Remove extraneous output

--- scripts/commands/9_Help.js
+++ scripts/commands/9_Help.js
@@ -7,11 +7,6 @@ var script9 = function(self, actor, here, args, extra) {
 	var searchTerm = vArgs.join(" ");
 	var hideFooter = false;
 
-	if(actor.level > 100) {
-		actor.send("This is a test2");
-		actor.send("Second message.");
-	}
-
 	if (searchTerm.length > 0) {
 		var file = global.helpManager.getSearchedFile(searchTerm);
 		if (!file)
