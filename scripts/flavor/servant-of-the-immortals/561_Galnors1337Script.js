ac2c86c711490d9e09e2d32ffbefc2e458860caa
Michael Mason <mikemason930@gmail.com>
2020-09-25 22:36:58 -0400

    Reformat script function

--- scripts/flavor/servant-of-the-immortals/561_Galnors1337Script.js
+++ scripts/flavor/servant-of-the-immortals/561_Galnors1337Script.js
@@ -1,21 +1,170 @@
-var script561 = function(self, actor, here, args, extra) {
-	waitpulse 1
-	var wA1 = ["phenomenal", "rapid", "chilling", "insipid", "nauseating", "astronomical", "austere", "inevitable", "inescapable", "reckless", "haphazard", "accelerating", "profound", "awesome", "terrifying", "ubiquitous","ignominious","unprecedented","unparalleled","insidious","broad","ridiculous","Norum-like"];
-	var wA2 = ["growth", "decline", "prospects", "acceleration", "threat", "expansion", "oneness", "outgrowth", "madness", "evacuation", "diminishment", "consumption", "decay", "putrefaction", "vapidity", "downsizing", "degeneration", "litigation", "declivity", "hastening", "paradigm shifting", "plague"];
-	var wA3 = ["Ghob'lin skill", "local coin", "Borderguards attitude", "Aes Sedai mentality", "censorship", "Wolfbrother bonuses", "Gaidin equipment", "rampant cheating", "Rhollor's appearance", "Galnor's coding", "trolloc illiteracy", "Lithium's stabbing", "Osis' skill", "Zarth's thinking", "Serai's help", "elitist mentality",
-	"the Fal Dara justice system", "total lack of change", "Gristlyn's words", "Kho'mon lack of resources", "current legends list", "Prism's lame tactics", "Norum's insanity", "Tyrak's suicidal intentions", "Gristlyn's typing skills", "Zyrak's brain power", "Jonlin's cheating", "Dobry's honor", "Atkins' reputation on Wheel of Time MUD",
-	"Kho'mon reputation", "fades/dreadlords that cannot PK", "Tulon's cheating", "Kison's anger", "Kison's videos", "the fact that the codebase is Circle", "Rhollor's building skills", "Ravir's activeness", "garbage on the forums", "advance in technology", "new medical treatments"];
-	var wA4 = ["undermine", "intensify", "demoralize", "evaporate", "dehumanize", "hasten", "incapacitate", "destabilize", "sabotage", "nullify", "truncate", "enchant", "enslave", "put an end to", "uglify", "revolutionize", "deteriorate", "overhaul", "aggrandize", "escalate", "emancipate"];
-	var wA5 = ["today's", "tomorrow's", "the entrenchment of our", "yesterday's", "our children's", "modern", "all of our", "our future", "our", "the demise of our", "our grandchildren's", "all hope for"];
-	var wA6 = ["trolloc", "re-ruralization", "self-worth", "trolloc condition", "self-esteem", "player psyche", "human psyche", "mental depth", "players attitude", "current sexual trends", "future on this mud", "hopes and dreams", "future conditions", "political climate", "lack of education", "cultural inadequecy", "sexual lifestyles",
-	"greed, need, and stinginess", "genetic diversity", "player mentality", "mental stability"];
-	
-	var w1 = wA1[random(0,wA1.length-1)];
-	var w2 = wA2[random(0,wA2.length-1)];
-	var w3 = wA3[random(0,wA3.length-1)];
-	var w4 = wA4[random(0,wA4.length-1)];
-	var w5 = wA5[random(0,wA5.length-1)];
-	var w6 = wA6[random(0,wA6.length-1)];
-	
-	self.say("The " + w1 + " " + w2 + " of " + w3 + " will " + w4 + " " + w5 + " " + w6 + ", " + actor.name);
-}
+let script561 = function(self, actor, here, args, extra) {
+
+	setTimeout(1, function() {
+		let wA2 = [
+			"growth",
+			"decline",
+			"prospects",
+			"acceleration",
+			"threat",
+			"expansion",
+			"oneness",
+			"outgrowth",
+			"madness",
+			"evacuation",
+			"diminishment",
+			"consumption",
+			"decay",
+			"putrefaction",
+			"vapidity",
+			"downsizing",
+			"degeneration",
+			"litigation",
+			"declivity",
+			"hastening",
+			"paradigm shifting",
+			"plague"
+		];
+
+		let wA1 = [
+			"phenomenal",
+			"rapid",
+			"chilling",
+			"insipid",
+			"nauseating",
+			"astronomical",
+			"austere",
+			"inevitable",
+			"inescapable",
+			"reckless",
+			"haphazard",
+			"accelerating",
+			"profound",
+			"awesome",
+			"terrifying",
+			"ubiquitous",
+			"ignominious",
+			"unprecedented",
+			"unparalleled",
+			"insidious",
+			"broad",
+			"ridiculous",
+			"Norum-like"
+		];
+
+		let wA3 = [
+			"Ghob'lin skill",
+			"local coin",
+			"Borderguards attitude",
+			"Aes Sedai mentality",
+			"censorship",
+			"Wolfbrother bonuses",
+			"Gaidin equipment",
+			"rampant cheating",
+			"Rhollor's appearance",
+			"Galnor's coding",
+			"trolloc illiteracy",
+			"Lithium's stabbing",
+			"Osis' skill",
+			"Zarth's thinking",
+			"Serai's help",
+			"elitist mentality",
+			"the Fal Dara justice system",
+			"total lack of change",
+			"Gristlyn's words",
+			"Kho'mon lack of resources",
+			"current legends list",
+			"Prism's lame tactics",
+			"Norum's insanity",
+			"Tyrak's suicidal intentions",
+			"Gristlyn's typing skills",
+			"Zyrak's brain power",
+			"Jonlin's cheating",
+			"Dobry's honor",
+			"Atkins' reputation on Wheel of Time MUD",
+			"Kho'mon reputation",
+			"fades/dreadlords that cannot PK",
+			"Tulon's cheating",
+			"Kison's anger",
+			"Kison's videos",
+			"the fact that the codebase is Circle",
+			"Rhollor's building skills",
+			"Ravir's activeness",
+			"garbage on the forums",
+			"advance in technology",
+			"new medical treatments"
+		];
+
+		let wA4 = [
+			"undermine",
+			"intensify",
+			"demoralize",
+			"evaporate",
+			"dehumanize",
+			"hasten",
+			"incapacitate",
+			"destabilize",
+			"sabotage",
+			"nullify",
+			"truncate",
+			"enchant",
+			"enslave",
+			"put an end to",
+			"uglify",
+			"revolutionize",
+			"deteriorate",
+			"overhaul",
+			"aggrandize",
+			"escalate",
+			"emancipate"
+		];
+
+		let wA5 = [
+			"today's",
+			"tomorrow's",
+			"the entrenchment of our",
+			"yesterday's",
+			"our children's",
+			"modern",
+			"all of our",
+			"our future",
+			"our",
+			"the demise of our",
+			"our grandchildren's",
+			"all hope for"
+		];
+
+		let wA6 = [
+			"trolloc",
+			"re-ruralization",
+			"self-worth",
+			"trolloc condition",
+			"self-esteem",
+			"player psyche",
+			"human psyche",
+			"mental depth",
+			"players attitude",
+			"current sexual trends",
+			"future on this mud",
+			"hopes and dreams",
+			"future conditions",
+			"political climate",
+			"lack of education",
+			"cultural inadequecy",
+			"sexual lifestyles",
+			"greed, need, and stinginess",
+			"genetic diversity",
+			"player mentality",
+			"mental stability"
+		];
+
+		let w1 = wA1[random(0,wA1.length-1)];
+		let w2 = wA2[random(0,wA2.length-1)];
+		let w3 = wA3[random(0,wA3.length-1)];
+		let w4 = wA4[random(0,wA4.length-1)];
+		let w5 = wA5[random(0,wA5.length-1)];
+		let w6 = wA6[random(0,wA6.length-1)];
+
+		self.say("The " + w1 + " " + w2 + " of " + w3 + " will " + w4 + " " + w5 + " " + w6 + ", " + actor.name);
+	});
+};
