0f114764e6c3bf0f81c0263a11890140f40cc5ba
Michael Mason <mikemason930@gmail.com>
2021-05-15 18:53:56 -0400

    SSH keyscan for git

--- docker-start.sh
+++ docker-start.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-
-mkdir -p /root/.ssh
-
 SSH_DIR_PATH="/root/.ssh";
 SSH_CONFIG_FILE_PATH="$SSH_DIR_PATH/config";
 SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH="$SSH_DIR_PATH/id_rsa_kinslayer_mud";
 GIT_HOSTNAME=`git -C /kinslayer/ remote -v | head -1 | awk -F'@' '{print $2}' | awk -F':' '{print $1}'`
 
-rm -f $SSH_CONFIG_FILE_PATH;
-rm -f $SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH;
+mkdir -p "$SSH_DIR_PATH"
 
-echo "Host $GIT_HOSTNAME" >> $SSH_CONFIG_FILE_PATH
-echo "Hostname github.com" >> $SSH_CONFIG_FILE_PATH
-echo "IdentityFile=/root/.ssh/id_rsa_kinslayer_mud" >> $SSH_CONFIG_FILE_PATH
+rm -f "$SSH_CONFIG_FILE_PATH";
+rm -f "$SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH";
 
-echo "$GIT_SSH_KEY" > $SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH
+echo "Host $GIT_HOSTNAME" >> "$SSH_CONFIG_FILE_PATH"
+echo "Hostname github.com" >> "$SSH_CONFIG_FILE_PATH"
+echo "IdentityFile=/root/.ssh/id_rsa_kinslayer_mud" >> "$SSH_CONFIG_FILE_PATH"
 
-chmod 700 $SSH_DIR_PATH;
-chmod 600 $SSH_CONFIG_FILE_PATH
-chmod 600 $SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH
+echo "$GIT_SSH_KEY" > "$SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH"
+
+chmod 700 "$SSH_DIR_PATH"
+chmod 600 "$SSH_CONFIG_FILE_PATH"
+chmod 600 "$SSH_KINSLAYER_MUD_GIT_KEY_FILE_PATH"
+ssh-keyscan github.com> >> "$SSH_DIR_PATH""/known_hosts"
 
 
 TARGET="$1"
