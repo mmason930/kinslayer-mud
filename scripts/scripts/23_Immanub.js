2feb35b73d036fc0ee7ca0a3d302c1d86bbf784f
Michael Mason <mikemason930@gmail.com>
2022-02-03 18:59:26 -0500

    Test import

--- scripts/scripts/23_Immanub.js
+++ scripts/scripts/23_Immanub.js
@@ -3,4 +3,4 @@ var script23 = function(self, actor, here, args, extra) {
 	here.echo("Ziltoid's Minion snaps to attention and salutes smartly.");
 	waitpulse 3;
 	here.echo("Ziltoid's Minion says, 'Galnor is a Steelers fan, and he is lame, Sir.'");
-}
+}// This is a test
