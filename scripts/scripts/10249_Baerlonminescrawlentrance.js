8e7b27297c117eac4791e7a70f95e69c6e613e15
Michael Mason <mikemason930@gmail.com>
2025-04-11 22:54:10 -0400

    Add additional Baerlon mine crawl keywords and error messaging

--- scripts/scripts/10249_Baerlonminescrawlentrance.js
+++ scripts/scripts/10249_Baerlonminescrawlentrance.js
@@ -1,36 +1,38 @@
 var script10249 = function(self, actor, here, args, extra) {
 	var vArgs = getArgList(args);
 	_block;
-	if( actor.position != constants.POS_STANDING )
+	if( actor.position !== constants.POS_STANDING )
 	{
-	        actor.send("You're not in the correct position.");
-	        return;
+		actor.send("You're not in the correct position.");
+		return;
 	}
-	if( strn_cmp(vArgs[1],"entrance",4) )
+	if( strn_cmp(vArgs[1],"entrance",4) || strn_cmp(vArgs[1],"in",2) )
 	{
-	        actor.send("You crawl through the mine's entrance.");
-	        echoaround(actor, actor.name + " crawls into the mine's entrance.");
-	        actor.moveToRoom(getRoom(10250));
-	        getRoom(10250).echoaround(actor, actor.name + " crawls in from outside.");
-	        actor.comm("look");
-	for (var _autoKey in actor.followers ) {
-		var follower = actor.followers [_autoKey];
-	        if( follower.position != constants.POS_STANDING )
-	        {
-	                continue;
-	        }
-	        if( follower.room != here )
-	        {
-	                continue;
-	        }
-	        else
-	        {
-	                act( "$n follows $N through the mine's entrance.", true, follower, null, actor, constants.TO_NOTVICT );
-	                act( "You follow $N through the mine's entrance.", true, follower, null, actor, constants.TO_CHAR );
-	                follower.moveToRoom(getRoom(10250));
-	                echoaround( follower, capFirstLetter(follower.name) + " crawls in from outside." );
-	                follower.comm( "look" );
-	        }
-	 }
+		actor.send("You crawl through the mine's entrance.");
+		echoaround(actor, actor.name + " crawls into the mine's entrance.");
+		actor.moveToRoom(getRoom(10250));
+		getRoom(10250).echoaround(actor, actor.name + " crawls in from outside.");
+		actor.comm("look");
+		for (var _autoKey in actor.followers ) {
+			var follower = actor.followers [_autoKey];
+			if( follower.position !== constants.POS_STANDING )
+			{
+				continue;
+			}
+			if( follower.room !== here )
+			{
+				continue;
+			}
+			else
+			{
+				act( "$n follows $N through the mine's entrance.", true, follower, null, actor, constants.TO_NOTVICT );
+				act( "You follow $N through the mine's entrance.", true, follower, null, actor, constants.TO_CHAR );
+				follower.moveToRoom(getRoom(10250));
+				echoaround( follower, capFirstLetter(follower.name) + " crawls in from outside." );
+				follower.comm( "look" );
+			}
+		}
+	} else {
+		actor.send("Crawl where?");
 	}
 }
--- scripts/scripts/10250_Baerlonminescrawlout.js
+++ scripts/scripts/10250_Baerlonminescrawlout.js
@@ -1,36 +1,38 @@
 var script10250 = function(self, actor, here, args, extra) {
 	var vArgs = getArgList(args);
 	_block;
-	if( actor.position != constants.POS_STANDING )
+	if( actor.position !== constants.POS_STANDING )
 	{
-	        actor.send("You're not in the correct position.");
-	        return;
+		actor.send("You're not in the correct position.");
+		return;
 	}
-	if( strn_cmp(vArgs[1],"out",2) )
+	if( strn_cmp(vArgs[1],"out",2) || strn_cmp(vArgs[1],"entrance",2) )
 	{
-	        actor.send("You crawl out of the mines.");
-	        echoaround(actor, actor.name + " crawls out through the mine's entrance.");
-	        actor.moveToRoom(getRoom(10249));
-	        getRoom(10249).echoaround(actor, actor.name + " crawls out of the mines.");
-	        actor.comm("look");
-	for (var _autoKey in actor.followers ) {
-		var follower = actor.followers [_autoKey];
-	        if( follower.position != constants.POS_STANDING )
-	        {
-	                continue;
-	        }
-	        if( follower.room != here )
-	        {
-	                continue;
-	        }
-	        else
-	        {
-	                act( "$n follows $N out through the mine's entrance.", true, follower, null, actor, constants.TO_NOTVICT );
-	                act( "You follow $N out through the mine's entrance.", true, follower, null, actor, constants.TO_CHAR );
-	                follower.moveToRoom(getRoom(10249));
-	                echoaround( follower, capFirstLetter(follower.name) + " crawls out of the mines." );
-	                follower.comm( "look" );
-	        }
-	 }
+		actor.send("You crawl out of the mines.");
+		echoaround(actor, actor.name + " crawls out through the mine's entrance.");
+		actor.moveToRoom(getRoom(10249));
+		getRoom(10249).echoaround(actor, actor.name + " crawls out of the mines.");
+		actor.comm("look");
+		for (var _autoKey in actor.followers ) {
+			var follower = actor.followers [_autoKey];
+			if( follower.position !== constants.POS_STANDING )
+			{
+				continue;
+			}
+			if( follower.room !== here )
+			{
+				continue;
+			}
+			else
+			{
+				act( "$n follows $N out through the mine's entrance.", true, follower, null, actor, constants.TO_NOTVICT );
+				act( "You follow $N out through the mine's entrance.", true, follower, null, actor, constants.TO_CHAR );
+				follower.moveToRoom(getRoom(10249));
+				echoaround( follower, capFirstLetter(follower.name) + " crawls out of the mines." );
+				follower.comm( "look" );
+			}
+		}
+	} else {
+		actor.send("Crawl where?");
 	}
 }
