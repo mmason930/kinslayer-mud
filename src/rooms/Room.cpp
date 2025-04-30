a561d18a42eaf320e01a8b63c01d0ea966cf6b98
Michael Mason <mikemason930@gmail.com>
2025-04-30 07:47:16 -0400

    Fix room trackable check

--- src/rooms/Room.cpp
+++ src/rooms/Room.cpp
@@ -60,7 +60,7 @@ Object *Room::findFirstObject(const std::function<bool(class Object *obj)> &pred
 
 bool Room::isTrackable()
 {
-	return (this->getSector() == RoomSector::inside && !ROOM_FLAGGED(this, ROOM_NOTRACK));
+	return (this->getSector() != RoomSector::inside && !ROOM_FLAGGED(this, ROOM_NOTRACK));
 }
 
 std::string Room::getDisplayableId()
