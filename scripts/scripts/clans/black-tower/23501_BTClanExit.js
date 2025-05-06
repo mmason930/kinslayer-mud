c155fcdfbf41d3b06e951318b84f45d53b4fb208
Michael Mason <mikemason930@gmail.com>
2025-05-06 15:58:02 -0400

    Reorganizing Black Tower scripts

--- scripts/scripts/clans/black-tower/23501_BTClanExit.js
+++ scripts/scripts/clans/black-tower/23501_BTClanExit.js
@@ -1,10 +1,12 @@
 var script23501 = function(self, actor, here, args, extra) {
-	var otherRoom = here.direction(constants.SOUTH);
+	let otherRoom = here.direction(constants.SOUTH);
 	wait 2
+
 	here.echo("An iron bar is lifted from the doors and they swing open soundlessly.");
 	otherRoom.echo( "The " + otherRoom.doorName(constants.NORTH) + " is opened from the other side." );
 	here.openDoor(constants.SOUTH,true);
 	wait 3
+
 	here.echo("The doors swing shut and the bar is replaced, locking them from intruders.");
 	otherRoom.echo( "The " + otherRoom.doorName(constants.NORTH) + " is closed from the other side." );
 	here.closeDoor(constants.SOUTH,true);
