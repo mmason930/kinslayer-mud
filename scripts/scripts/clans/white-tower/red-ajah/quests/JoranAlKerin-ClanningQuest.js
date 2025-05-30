e30fb482570af4edd788c1c5eec99aa7ab340ee4
Michael Mason <mikemason930@gmail.com>
2025-05-29 20:20:01 -0400

    Red Ajah apprenticeship channeling

--- scripts/scripts/clans/white-tower/red-ajah/quests/JoranAlKerin-ClanningQuest.js
+++ scripts/scripts/clans/white-tower/red-ajah/quests/JoranAlKerin-ClanningQuest.js
@@ -1,5 +1,27 @@
 let script13233 = function(self, actor, here, args, extra) {
 
+    if(self.fighting) {
+
+        const channelingRoll = random(1, 100);
+
+        if(channelingRoll <= 25) {
+            act("$n extends $s arms towards the ground, which begins shaking violently!", false, self, null, null, constants.TO_ROOM);
+            act("You extend your arms towards the ground, which begins shaking violently!", false, self, null, null, constants.TO_CHAR);
+            here.people.forEach(function (personInRoom) {
+                if (personInRoom.isValid && personInRoom.fighting === self) {
+                    personInRoom.damage(random(15, 40), self);
+                }
+            });
+        } else if(channelingRoll <= 50) {
+            act("$n extends his arms outwards towards $N, and a fireball fires from $s hands!", false, self, null, null, constants.TO_NOTVICT);
+            act("$n extends his arms outwards towards you, and a fireball fires from $s hands!", false, self, null, null, constants.TO_VICT);
+            act("You extend your arms outwards towards $N and a fireball fires from your hands!", false, self, null, null, constants.TO_CHAR);
+            self.fighting.damage(random(35, 75), self);
+        }
+
+        return;
+    }
+    
     const seenPlayers = here.people.filter(function(personInRoom) {
         if(!personInRoom.isMob() && self.canSee(personInRoom)) {
             return true;
@@ -39,6 +61,8 @@ let script13233 = function(self, actor, here, args, extra) {
         ]
     }
 
-    commandsToUse = commandsToUse[random(0, commandsToUse.length - 1)];
-    self.comm(commandsToUse);
+    if(commandsToUse !== null) {
+        commandsToUse = commandsToUse[random(0, commandsToUse.length - 1)];
+        self.comm(commandsToUse);
+    }
 }
